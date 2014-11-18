39642a8a2069c3571ada6487a953ca2b16cfc74e
Shane Harvey <shane6harvey@yahoo.com>
2014-11-18 14:38:24 -0500

    Common
    Changed server/odr files to be in /tmp

--- common.h
+++ common.h
@@ -1,8 +1,8 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#define SERVER_PATH  "./server.tmp"
-#define ODR_PATH "/home/cse533-14/ODR.tmp"
+#define SERVER_PATH  "/tmp/server_cse533-14.tmp"
+#define ODR_PATH     "/tmp/ODR_cse533-14.tmp"
 #define SERVER_PORT 8080
 
 #endif
